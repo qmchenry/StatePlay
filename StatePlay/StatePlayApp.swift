1dc48bd7fa959299792ee7b38c54261c825467df
Quinn McHenry <quinn.mchenry@smallplanet.com>
2021-12-14 20:29:22 -0500

    Initial Commit

---
+++ StatePlay/StatePlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePlayApp.swift
+//  StatePlay
+//
+//  Created by Quinn McHenry on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct StatePlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
